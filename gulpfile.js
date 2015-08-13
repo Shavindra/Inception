d39975c636a489c26106aef1212999fa744f0805
Shavin Fonseka <shavindra2001@yahoo.co.uk>
2015-08-13 22:35:29 +0100

    Refactored some HTML

---
+++ gulpfile.js
@@ -0,0 +1,15 @@
+﻿var gulp = require('gulp'),
+    fs = require("fs"),
+    requireDir = require('require-dir');
+
+requireDir('Tasks', { recurse: true });
+
+gulp.task('default', function () {
+    // place code for your default task here
+});
+
+gulp.task('debug', function () {
+    console.log(taskConfig);
+    console.log(fs);
+
+})
---
+++ src/Directives/Inc-Directives/README.md
@@ -0,0 +1,3 @@
+﻿# Inc-Directives
+
+
